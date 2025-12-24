5fccc65463762fb394a4f25f0a765bcb210e4221
婉瑜 <c1538411@gmail.com>
2025-12-24 14:03:23 +0800

    Refactor form validation for clarity and checks

--- 01.js
+++ 01.js
@@ -1,18 +1,28 @@
 // 老師教的簡單表單驗證
 function validateForm() {
-    var name = document.forms[0].name.value;
-    var email = document.forms[0].email.value;
-    
+    var form = document.forms[0];
+    var name = form.name.value;
+    var email = form.email.value;
+
     if (name == "" || email == "") {
-        alert("請填寫姓名與 Email！");
+        alert("請先填寫姓名與 Email");
         return false;
     }
-    
-    if (!email.includes("@")) {
-        alert("請輸入正確的 Email 格式！");
+
+    // 檢查有沒有至少勾一個商品
+    var items = form.products;  // 這裡對應 name="products"
+    var checked = false;
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].checked) {
+            checked = true;
+            break;
+        }
+    }
+    if (!checked) {
+        alert("請至少勾選一項想購買的商品");
         return false;
     }
-    
-    alert("訂單已送出！我會盡快與您聯繫，謝謝！");
+
+    alert("訂單已送出！感謝您的填寫～");
     return true;
 }
